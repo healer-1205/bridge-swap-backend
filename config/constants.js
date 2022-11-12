6d2a4f6e2e3fc4d7645f998c0e139f9f9e548107
healer-1205 <crazyhealer1205@gmail.com>
2022-11-12 11:26:53 +0800

    add kucoin sdk module and configure kucoin constants

--- config/constants.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  base_url: "https://api.kucoin.com",
-}
---
+++ config/kucoin.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  baseUrl: "https://api.kucoin.com",
+  apiAuth: {
+    key: "6178e43fb094040001092a40", // KC-API-KEY
+    secret: "", // API-Secret
+    passphrase: "", // KC-API-PASSPHRASE
+  },
+  authVersion: 2, // KC-API-KEY-VERSION. Notice: for v2 API-KEY, not required for v1 version.
+}
